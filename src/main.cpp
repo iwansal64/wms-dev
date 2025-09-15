35379fb3ed597ddb8df6f66fcf7997e4b18b0bfa
Iwansal64 <iwansal64@gmail.com>
2025-09-15 18:39:28 +0700

    [flag] Web Socket Communication Success!

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,52 @@
 
 #include <env.h>
 
+#define INTERVAL_PER_DATA 2000
+
+uint64_t last_time_update_data = 0;
+
 WebSocketManager ws_manager;
 
+void on_websocket_data(WEBSOCKET_DATA);
+void check_water_leakage();
+
+void setup() {  
+  Serial.begin(9600);
+
+  #ifdef SHOW_DEBUG
+  Serial.print("[WIFI] Connecting to ");
+  Serial.println(ENV_WIFI_SSID);
+  #endif
+
+  WiFi.begin(ENV_WIFI_SSID, ENV_WIFI_PASS);
+  while(WiFi.status() != WL_CONNECTED) delay(500);
+
+  #ifdef SHOW_DEBUG
+  Serial.println("[WIFI] Connected!");
+  #endif
+  
+  #ifdef SHOW_DEBUG
+  Serial.print("[WiFi] IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("[WiFi] Default Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+  #endif
+  
+  ws_manager.init(ENV_WS_ADDR, (uint16_t) 8040);
+}
+
+void loop() {
+  ws_manager.loop();
+  uint64_t elapsed = millis() - last_time_update_data;
+  if(elapsed > INTERVAL_PER_DATA) {
+    #ifdef SHOW_INFO
+    Serial.println("[MAIN] Checking Water Leakage");
+    #endif
+    check_water_leakage();
+    Serial.println(elapsed);
+    last_time_update_data = millis();
+  }
+}
 
 void on_websocket_data(WEBSOCKET_DATA) {
   switch (type) {
@@ -14,7 +58,7 @@ void on_websocket_data(WEBSOCKET_DATA) {
 
     case WStype_CONNECTED:
       Serial.printf("Connected to: %s\n", payload);
-      ws_manager.print("Hello Server");
+      ws_manager.put("Hello Server");
       break;
 
     case WStype_TEXT:
@@ -27,17 +71,15 @@ void on_websocket_data(WEBSOCKET_DATA) {
   }
 }
 
-void setup() {  
-  Serial.begin(9600);
-  Serial.println("Connecting..");
-  ws_manager.init(ENV_WIFI_SSID, ENV_WIFI_PASS, ENV_WS_ADDR, (uint16_t) 8040);
-  Serial.println("Connected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Default Gateway: ");
-  Serial.println(WiFi.gatewayIP());
-}
+void check_water_leakage() {
+  ws_manager.wait_to_connect();
+  
+  uint8_t water_leak_value = random(0, 4);
 
-void loop() {
-  ws_manager.loop();
+  // Prepare the data
+  ws_manager.put(String("leak="));
+  ws_manager.put(water_leak_value);
+
+  // Send the data
+  bool result = ws_manager.launch();
 }
